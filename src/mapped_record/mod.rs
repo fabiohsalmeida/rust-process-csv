d54208240adb720414b0bbe0b5be7b99908b340c
Fábio Almeida <fabio.h.s.almeida@gmail.com>
2024-03-31 11:46:18 -0300

    Including TODOs and tempo endpoint uri treatment

--- src/mapped_record/mod.rs
+++ src/mapped_record/mod.rs
@@ -1,5 +1,7 @@
 use csv::StringRecord;
 
+const KNOWED_HOSTS_TO_REPLACE: [&str; 4] = ["/bancodobrasil", "/bradesco", "/itau-itaucard", "/iti-itaucard"];
+
 #[derive(Debug)]
 pub struct MappedInputRecord {
     pub time: String,
@@ -21,7 +23,7 @@ impl MappedInputRecord {
             api_version: Self::get_version(&record[1]),
             http_host: (&record[2]).parse().unwrap(),
             http_method: (&record[3]).parse().unwrap(),
-            http_path: (&record[4]).parse().unwrap(),
+            http_path: Self::get_endpoint_uri(&record[4]),
             http_status_code: Self::get_status(&record[5]),
             provider_id: (&record[6]).parse().unwrap(),
             quantity: (&record[7]).parse().unwrap()
@@ -41,6 +43,7 @@ impl MappedInputRecord {
         return field.parse().unwrap();
     }
 
+    // TODO make it work independent from explicitly pattern
     fn get_version(
         field: &str
     ) -> String {
@@ -52,5 +55,20 @@ impl MappedInputRecord {
         }
 
         return field.parse().unwrap();
+    }
+
+    // TODO remove any host without list
+    fn get_endpoint_uri(
+        field: &str
+    ) -> String {
+        let to_pattern = "";
+
+        for host in KNOWED_HOSTS_TO_REPLACE {
+            if field.contains(host) {
+                return field.replace(host, to_pattern).trim().parse().unwrap();
+            }
+        }
+
+        return field.trim().parse().unwrap();
     }
 }
